a4e0ece2be01677ed592bafac08416f3da3c0092
Famer <1243204339@qq.com>
2019-10-16 17:31:30 +0800

    Update js.url.js

--- js.url.js
+++ js.url.js
@@ -85,6 +85,7 @@
     }
 
     function port(url) {
+        url = host(url);
         var matches = url.match(/:([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])/);
         return !!matches ? matches[1] : '';
     }
