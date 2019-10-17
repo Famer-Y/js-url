e3a62f55c070a52eebb1f5459f41609a10b84c32
Famer <1243204339@qq.com>
2019-10-17 11:07:52 +0800

    Update js.url.js

--- js.url.js
+++ js.url.js
@@ -95,7 +95,7 @@
     }
 
     function regexp(str) {
-        return str.replace(/([\$\(\)\*\+\.\[\?\\\^\{\|])/ig, '\\$1');
+        return str.replace(/([\$\(\)\*\+\.\[\?\\\^\{\|])/g, '\\$1');
     }
 
     function url(url) {
